2e7c4b073f5572898529358d88bd63127f317720
Paivy <paivyeshirera@gmail.com>
2022-05-09 10:21:07 +0300

    added bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
+# from ensurepip import bootstrap
+from trace import Trace
 from flask import Flask
+from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 #Initializing application
-
 app = Flask(__name__)
+
+#Setting up configuration
+app.config.from_object(DevConfig)
+# app.config.from_pyfile("config.py")
+
+#Initializing Flask extensions
+bootstrap = Bootstrap(app)
+
 
 from app import views

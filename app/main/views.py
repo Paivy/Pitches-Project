dadd33427c29060a9277d4dbf64570bbb547321a
Paivy <paivyeshirera@gmail.com>
2022-05-11 07:43:25 +0300

    resolved errors and backlogd

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,66 @@
 from flask import render_template
-from app import app
+# from app import app
+from . import main
+from flask_login import login_required,current_user
+from ..models import Pitch,User,Comment
+from flask.views import View,MethodView
+from .. import db
+# import markdown2
 
-#Vies
-@app.route('/')
+@main.route('/', methods = ['GET','POST'])
 def index():
-    """
-    View root page function that returns the index page and its data
-    """
 
-    message = 'Place your pitch'
-    title = 'Welcome to Pitches App'
-    return render_template('index.html', message = message, title = title)
+    '''
+    View root page function that returns the index page and its data
+    '''
+    pitch = Pitch.query.filter_by().first()
+    title = 'Home'
+    pickuplines = Pitch.query.filter_by(category="pickuplines")
+    interviewpitch = Pitch.query.filter_by(category = "interviewpitch")
+    promotionpitch = Pitch.query.filter_by(category = "promotionpitch")
+    productpitch = Pitch.query.filter_by(category = "productpitch")
+
+    # upvotes = Upvote.get_all_upvotes(pitch_id=Pitch.id)
+    
+
+    return render_template('index.html', title = title, pitch = pitch, pickuplines=pickuplines, interviewpitch= interviewpitch, promotionpitch = promotionpitch, productpitch = productpitch)
+    
+@main.route('/pitches/new/', methods = ['GET','POST'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    my_upvotes = Upvote.query.filter_by(pitch_id = Pitch.id)
+    if form.validate_on_submit():
+        description = form.description.data
+        title = form.title.data
+        owner_id = current_user
+        category = form.category.data
+        print(current_user._get_current_object().id)
+        new_pitch = Pitch(owner_id =current_user._get_current_object().id, title = title,description=description,category=category)
+        db.session.add(new_pitch)
+        db.session.commit()
+        
+        
+        return redirect(url_for('main.index'))
+    return render_template('pitches.html',form=form)
+
+
+
+
+@main.route('/comment/new/<int:pitch_id>', methods = ['GET','POST'])
+@login_required
+def new_comment(pitch_id):
+    form = CommentForm()
+    pitch=Pitch.query.get(pitch_id)
+    if form.validate_on_submit():
+        description = form.description.data
+
+        new_comment = Comment(description = description, user_id = current_user._get_current_object().id, pitch_id = pitch_id)
+        db.session.add(new_comment)
+        db.session.commit()
+
+
+        return redirect(url_for('.new_comment', pitch_id= pitch_id))
+
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+    return render_template('comments.html', form = form, comment = all_comments, pitch = pitch )
